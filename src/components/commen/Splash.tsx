6c2801e3cccf8538e0612e010dec055db00f6317
19Edwin92 <b_a_s_e@naver.com>
2023-08-19 00:42:25 +0900

    [Feat] Splash 이미지 파일 수정 및 디자인 적용

--- src/components/commen/Splash.tsx
+++ src/components/commen/Splash.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import { useMainHeader } from '../../hooks'
+import Lottie from 'react-lottie';
 import * as SC from '../css'
+import LottieData from '../../assets/logo/300_logo.json'
 
 export const Splash:React.FC = () => {
   const { SplashScreenRef } = useMainHeader()
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: LottieData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
   return (
     <SC.SplashScreen ref={SplashScreenRef}>
-    <img 
-      src={require('../../assets/SnappyWelloff.gif')} 
-      alt='SplashScreen' />
+
+      <Lottie
+        options={defaultOptions}
+        height={300}
+        width={300}
+        isClickToPauseDisabled={false}
+      />
   </SC.SplashScreen>
   )
 }
--- src/components/commen/index.ts
+++ src/components/commen/index.ts
@@ -2,3 +2,5 @@ export * from './Splash'
 export * from "./MainHeader"
 export * from './MoblieSiderbar'
 export * from './DeskTopNavbar'
+
+
