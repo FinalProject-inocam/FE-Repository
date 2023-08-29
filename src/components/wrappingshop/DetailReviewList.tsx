0dc1518c5acf4690f35c0be57884bcbb09fe6978
19Edwin92 <b_a_s_e@naver.com>
2023-08-29 19:49:31 +0900

    [HOXFIX] useInfinityThrottle관련 코드스플리팅 과정에서 남아있던 코드로 인한 에러 제어

--- src/components/wrappingshop/DetailReviewList.tsx
+++ src/components/wrappingshop/DetailReviewList.tsx
@@ -10,30 +10,11 @@ const ReviewInner: FC<{
 	reviews: any;
 	setPage?: any;
 }> = ({
-	reviews: { reviewId, nickname, star, revisit, createAt, shopId, review, imageUrls },
+	reviews: { reviewId, nickname, star, revisit, createAt, shopId, review, imageUrls }
 }) => {
 
-	const RefetchThrottle = (callback: () => void, delay: number) => {
-		let timeId: NodeJS.Timeout | null = null;
-		return () => {
-			if (timeId) return;
-			timeId = setTimeout(() => {
-				callback();
-				timeId = null;
-			}, delay);
-		};
-	};
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	const onNextPageCallback = useCallback(
-		RefetchThrottle(() => {
-			console.log("쓰로틀 시작");
-			setPage(page + 1);
-		}, 1000),
-		[]
-	);
-
 	return (
-		<SC.ReviewListLayout ref={fetchNextRef} $gtc='80px 1fr' $cgap={20}>
+		<SC.ReviewListLayout $gtc='80px 1fr' $cgap={20}>
 			<SC.CustomH3 $types='nickname'>{nickname}</SC.CustomH3>
 			<SC.FlexBox
 				$fd='column'
