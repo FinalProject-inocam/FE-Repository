dc52d64475338d9bc5a02316f97cee7234ecae9a
Edwin Park <b_a_s_e@naver.com>
2023-09-05 15:03:30 +0900

    [refector] 관리자페이지 레이아웃 리펙토링

--- src/components/atom/FigureObjectFitImg.tsx
+++ src/components/atom/FigureObjectFitImg.tsx
@@ -3,9 +3,9 @@ import { FigureObjectFit } from "../css";
 import * as Type from "../../types";
 
 // <FigureImg width={width} src={require(`../assets/frame-000.jpg`)} alt="SomeImg"/>
-export const FigureObjectFitImg: React.FC<Type.FigureImg> = ({ width, height, src, alt, types, children, shadow }) => {
+export const FigureObjectFitImg: React.FC<Type.FigureImg> = ({ width, height, src, alt, types, children, shadow, onClick }) => {
 	return (
-		<FigureObjectFit $width={width} $height={height} $types={types} $shadow={shadow}>
+		<FigureObjectFit $width={width} $height={height} $types={types} $shadow={shadow} onClick={onClick}>
 			<img src={src} alt={alt} />
 			{children}
 		</FigureObjectFit>
